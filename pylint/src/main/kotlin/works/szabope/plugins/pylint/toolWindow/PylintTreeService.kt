efaebd89300a1f4093454ab3f608ad2b5182a178
Peter Szabo <szabope@users.noreply.github.com>
2025-06-06 10:59:57 +0000

    common

--- pylint/src/main/kotlin/works/szabope/plugins/pylint/toolWindow/PylintTreeService.kt
+++
@@ -1,8 +0,0 @@
-package works.szabope.plugins.pylint.toolWindow
-
-import com.intellij.openapi.components.Service
-import works.szabope.plugins.common.toolWindow.AbstractTreeService
-import works.szabope.plugins.pylint.services.pylintSeverityConfigs
-
-@Service(Service.Level.PROJECT)
-class PylintTreeService : AbstractTreeService(pylintSeverityConfigs.keys)
--- pylint/src/main/kotlin/works/szabope/plugins/pylint/vcs/PylintCheckinHandlerFactory.kt
+++
@@ -1,9 +0,0 @@
-package works.szabope.plugins.pylint.vcs
-
-import com.intellij.openapi.vcs.CheckinProjectPanel
-import com.intellij.openapi.vcs.changes.CommitContext
-import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
-
-class PylintCheckinHandlerFactory : CheckinHandlerFactory() {
-    override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext) = PylintCheckinHandler(panel)
-}
