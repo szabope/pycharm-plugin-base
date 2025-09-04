22dee718239ca119774b4a8eaf07c9493348d067
Peter Szabo <szabope@users.noreply.github.com>
2025-09-04 09:50:51 +0000

    replace shell execution configuration

--- src/main/kotlin/works/szabope/plugins/common/run/CliExecutionEnvironmentFactory.kt
+++ src/main/kotlin/works/szabope/plugins/common/run/CliExecutionEnvironmentFactory.kt
@@ -5,14 +5,11 @@ import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.openapi.project.Project
-import com.intellij.sh.run.ShConfigurationType
-import com.intellij.sh.run.ShRunConfiguration
 
 class CliExecutionEnvironmentFactory(private val project: Project) {
     fun createEnvironment(command: String): ExecutionEnvironment {
-        val conf = ShConfigurationType.getInstance().createTemplateConfiguration(project) as ShRunConfiguration
+        val conf = ShConfigurationType.INSTANCE.createTemplateConfiguration(project) as ShRunConfiguration
         conf.scriptText = command
-        conf.isExecuteScriptFile = false
         val settings = RunManager.getInstance(project).createConfiguration(conf, conf.factory!!)
         settings.isActivateToolWindowBeforeRun = false
         val executor = DefaultRunExecutor.getRunExecutorInstance()
