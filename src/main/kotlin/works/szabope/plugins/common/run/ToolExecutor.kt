5088ede11eb72c099cb3d1311065b0ce1010af3d
Peter Szabo <szabope@users.noreply.github.com>
2025-10-14 07:19:56 +0000

    simplify tool execution api

--- src/main/kotlin/works/szabope/plugins/common/run/ToolExecutor.kt
+++ src/main/kotlin/works/szabope/plugins/common/run/ToolExecutor.kt
@@ -4,29 +4,29 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.ProcessOutput
 import com.intellij.execution.util.ExecUtil
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.projectRoots.Sdk
 import com.jetbrains.python.sdk.PythonExecuteUtils
+import com.jetbrains.python.sdk.pythonSdk
+import works.szabope.plugins.common.CommonBundle
 import works.szabope.plugins.common.services.ImmutableSettingsData
 import java.nio.file.Path
 import kotlin.time.Duration
 
-open class ToolExecutor(private val project: Project, private val pythonSdk: Sdk, private val moduleToRun: String) {
-    fun execute(configuration: ImmutableSettingsData, parameters: List<String> = emptyList()): ProcessOutput {
-        return if (configuration.useProjectSdk) {
-            PythonExecuteUtils.executePyModuleScript(
-                project,
-                pythonSdk,
-                moduleToRun,
-                parameters,
-                timeout = Duration.INFINITE
-            )
+abstract class ToolExecutor(private val project: Project, private val moduleToRun: String) {
+    fun execute(configuration: ImmutableSettingsData, parameters: List<String> = emptyList()): Result<ProcessOutput> {
+        if (configuration.useProjectSdk) {
+            val pythonSdk = requireNotNull(project.pythonSdk) { CommonBundle.message("tool_executor.python_sdk_null") }
+            return runCatching {
+                PythonExecuteUtils.executePyModuleScript(
+                    project, pythonSdk, moduleToRun, parameters, timeout = Duration.INFINITE
+                )
+            }
         } else {
             val commandLine = GeneralCommandLine()
             commandLine.withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
             commandLine.withWorkingDirectory(configuration.projectDirectory?.let { Path.of(it) })
             commandLine.withExePath(configuration.executablePath)
             commandLine.withParameters(parameters)
-            ExecUtil.execAndGetOutput(commandLine)
+            return runCatching { ExecUtil.execAndGetOutput(commandLine) }
         }
     }
 }
