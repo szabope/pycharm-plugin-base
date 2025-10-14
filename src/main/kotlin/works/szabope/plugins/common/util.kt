dd6ddd0bf0f0460ecf4c265a1a8f3fec0dc173ca
Peter Szabo <szabope@users.noreply.github.com>
2025-10-14 07:59:11 +0000

    make sure processErrorAndGet remains available

--- src/main/kotlin/works/szabope/plugins/common/util.kt
+++ src/main/kotlin/works/szabope/plugins/common/util.kt
@@ -1,4 +1,12 @@
 package works.szabope.plugins.common
 
+import java.util.concurrent.CancellationException
+
 // copied from com.intellij.grazie.utils.Utils
 fun String.trimToNull(): String? = trim().takeIf(String::isNotBlank)
+
+// copied from com.intellij.collaboration.util.resultUtil
+inline fun <T> Result<T>.processErrorAndGet(handler: (e: Throwable) -> Unit): T =
+    onFailure {
+        if (it !is CancellationException && it !is Error) handler(it)
+    }.getOrThrow()
