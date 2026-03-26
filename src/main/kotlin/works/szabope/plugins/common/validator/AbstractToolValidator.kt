68a6e0347c570dd2ae9b2f72cbb82a739f150218
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 17:39:54 +0100

    refactor tool validation and its messages

--- src/main/kotlin/works/szabope/plugins/common/validator/AbstractToolValidator.kt
+++ src/main/kotlin/works/szabope/plugins/common/validator/AbstractToolValidator.kt
@@ -1,42 +1,32 @@
 package works.szabope.plugins.common.validator
 
+import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.CapturingProcessHandler
-import com.intellij.execution.target.TargetedCommandLineBuilder
-import com.intellij.execution.target.local.LocalTargetEnvironment
-import com.intellij.execution.target.local.LocalTargetEnvironmentRequest
 import com.intellij.openapi.project.Project
 import com.jetbrains.python.packaging.PyPackage
 import works.szabope.plugins.common.services.AbstractPluginPackageManagementService
 import works.szabope.plugins.common.services.PluginPackageManagementException
 import java.io.File
 
-abstract class AbstractToolValidator(protected val project: Project) {
-
+abstract class AbstractToolValidator(protected val project: Project, private val messages: ToolValidatorMessages) {
     protected abstract val versionFlag: String
     protected abstract val packageName: String
     protected abstract fun getPackageManagementService(): AbstractPluginPackageManagementService
-
-    protected abstract fun pathNotExistsMessage(): String
-    protected abstract fun pathIsDirectoryMessage(): String
-    protected abstract fun pathNotExecutableMessage(): String
-    protected abstract fun unknownVersionMessage(): String
-    protected abstract fun invalidVersionMessage(): String
-    protected abstract fun notInstalledMessage(): String
 
     fun validateExecutablePath(path: String?): String? {
         val p = path ?: return null
         require(p.isNotBlank())
         val file = File(p)
-        if (!file.exists()) return pathNotExistsMessage()
-        if (file.isDirectory) return pathIsDirectoryMessage()
-        if (!file.canExecute()) return pathNotExecutableMessage()
+        if (!file.exists()) return messages.pathNotExists
+        if (file.isDirectory) return messages.pathIsDirectory
+        if (!file.canExecute()) return messages.pathNotExecutable
         return null
     }
 
     fun validateVersion(path: String): String? {
-        val version = getVersionForExecutable(path) ?: return unknownVersionMessage()
+        val version = getVersionForExecutable(path) ?: return messages.unknownVersion
         if (!getPackageManagementService().getRequirement().match(PyPackage(packageName, version))) {
-            return invalidVersionMessage()
+            return messages.invalidVersion
         }
         return null
     }
@@ -44,28 +34,17 @@ abstract class AbstractToolValidator(protected val project: Project) {
     suspend fun validateProjectSdk(): String? {
         getPackageManagementService().checkInstalledRequirement().onFailure {
             when (it) {
-                is PluginPackageManagementException.PackageNotInstalledException -> return notInstalledMessage()
-                is PluginPackageManagementException.PackageVersionObsoleteException -> return invalidVersionMessage()
+                is PluginPackageManagementException.PackageNotInstalledException -> return messages.notInstalled
+                is PluginPackageManagementException.PackageVersionObsoleteException -> return messages.invalidVersion
             }
         }
         return null
     }
 
     protected fun getVersionForExecutable(pathToExecutable: String): String? {
-        val targetEnvRequest = LocalTargetEnvironmentRequest()
-        val targetEnvironment = LocalTargetEnvironment(LocalTargetEnvironmentRequest())
-
-        @Suppress("UnstableApiUsage") val commandLineBuilder = TargetedCommandLineBuilder(targetEnvRequest)
-        commandLineBuilder.setExePath(pathToExecutable)
-        commandLineBuilder.addParameters(versionFlag)
-
-        val targetCMD = commandLineBuilder.build()
-        val process = targetEnvironment.createProcess(targetCMD)
-
+        val commandLine = GeneralCommandLine(pathToExecutable, versionFlag)
         return runCatching {
-            @Suppress("UnstableApiUsage") val processHandler = CapturingProcessHandler(
-                process, targetCMD.charset, targetCMD.getCommandPresentation(targetEnvironment)
-            )
+            val processHandler = CapturingProcessHandler(commandLine)
             val processOutput = processHandler.runProcess(5000, true).stdout
             "(\\d+.\\d+.\\d+)".toRegex().find(processOutput)?.groups?.last()?.value
         }.getOrNull()
