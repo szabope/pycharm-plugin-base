fef269bdf8aced3ae66f7887fc22a0142fbbb806
Peter Szabo <szabope@users.noreply.github.com>
2026-03-27 12:15:04 +0100

    minor kotlinification

--- src/main/kotlin/works/szabope/plugins/common/CommonBundle.kt
+++ src/main/kotlin/works/szabope/plugins/common/CommonBundle.kt
@@ -9,7 +9,7 @@ private const val BUNDLE = "messages.works_szabope_plugins_CommonBundle"
 
 object CommonBundle {
 
-    private val bundle = DynamicBundle(CommonBundle.javaClass, BUNDLE)
+    private val bundle = DynamicBundle(CommonBundle::class.java, BUNDLE)
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = bundle.getMessage(key, *params)
