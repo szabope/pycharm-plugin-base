fac05a2b55929e1f758a298406e0af1cd86b4fa1
Peter Szabo <szabope@users.noreply.github.com>
2026-03-27 10:52:43 +0100

    clean up ToolProcessHandler

--- src/main/kotlin/works/szabope/plugins/common/run/ToolProcessHandler.kt
+++ src/main/kotlin/works/szabope/plugins/common/run/ToolProcessHandler.kt
@@ -4,17 +4,12 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.OSProcessHandler
 import com.intellij.util.io.BaseDataReader
 import com.intellij.util.io.BaseOutputReader
-import java.lang.Boolean
-import java.nio.charset.Charset
-import kotlin.String
 
-class ToolProcessHandler : OSProcessHandler {
-    constructor(commandLine: GeneralCommandLine) : super(commandLine)
-    constructor(process: Process, commandLine: String, charset: Charset) : super(process, commandLine, charset)
+class ToolProcessHandler(commandLine: GeneralCommandLine) : OSProcessHandler(commandLine) {
 
     override fun readerOptions() = object : BaseOutputReader.Options() {
         override fun policy(): BaseDataReader.SleepingPolicy {
-            return if (Boolean.getBoolean("output.reader.blocking.mode")) {
+            return if (System.getProperty("output.reader.blocking.mode", "false").toBoolean()) {
                 BLOCKING
             } else {
                 NON_BLOCKING
